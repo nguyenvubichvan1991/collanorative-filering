4af14950c0013f907064609c064d65988ead664f
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-15 18:16:25 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pickle 
+from flask import Flask, abort, jsonify, request
+app = Flask(__name__)
+
+#@app.route("/")
+def hello():
+    return "Welcom to the class DLB16HT201!!"
