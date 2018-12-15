2b6009935df34b567742e96373c1674308c0824d
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-15 17:59:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+import numpy as np
+import pickle 
+from flask import Flask, abort, jsonify, request
 app = Flask(__name__)
-
-@app.route("/")
 def hello():
     return "Welcom to the class DLB16HT201!!"
+load_model = pickle.load(open("https://nguyenvubichvan030.herokuapp.com/data/tblusers.csv","rb"))
+app = Flask(__name__)
+@app.route('/predict', methods=['POST'])
+def predict():
+     # Error checking
+     data = request.get_json(force=True)
+     # Convert JSON to numpy array
+     predict_request = [data['user_id'],data['nlid'],data['rating'],data['unix_timestamp']]
+     predict_request = np.array(predict_request)
+     # Predict using the random forest model
+     y = load_model.predict(predict_request)
+     # Return prediction
+     output = [y[0]]
+     return jsonify(results=output)
