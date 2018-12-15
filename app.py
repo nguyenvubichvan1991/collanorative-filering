436763183925bb832fc4ef5aac445fc16cddce8f
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-15 18:04:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import numpy as np
 import pickle 
 from flask import Flask, abort, jsonify, request
 app = Flask(__name__)
-def hello():
-    return "Welcom to the class DLB16HT201!!"
 load_model = pickle.load(open("../data/schools.csv","rb"))
-app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
      # Error checking
