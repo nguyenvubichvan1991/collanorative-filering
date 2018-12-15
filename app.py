cdf50eaa91bf5f506a69746cc422f26e8c38d51b
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-15 18:18:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pickle
 from flask import Flask, abort, jsonify, request
 app = Flask(__name__)
 
-#@app.route("/")
+@app.route("/")
 def hello():
     return "Welcom to the class DLB16HT201!!"
